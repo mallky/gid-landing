62ae6f5ede6ebd324a25f6e4520c6e4168b603c5
mallky <makarkuz@yandex.ru>
2018-03-28 17:12:56 +0400

    (fix) remove test prefix

--- send.php
+++ send.php
@@ -1,8 +1,8 @@
 <?php
 if (isset($_POST['phone'])) {    
     $to  = "info@gidizhevsk.com";
-    $subject = "Заказ звонка (ЭТО ТЕСТ)"; 
-    $message = '(ЭТО ТЕСТ) Телефон заказчика:'.$_POST['phone']; 
+    $subject = "Заказ звонка"; 
+    $message = 'Телефон заказчика:'.$_POST['phone']; 
     $headers  = "Content-type: text/html; charset=utf-8 \r\n";
 
     mail($to, $subject, $message, $headers);
